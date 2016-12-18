1ce2fff29d7c6d0408764a0ed826dec113f7e9e4
Rutger de Kok <de.kok.rutger@gmail.com>
2016-12-18 21:12:22 +0100

    Eerste checkin van Boer Kaas Applicatie

---
+++ Workshop1/KaasRelaas/src/Controller/kaasRelaasMain.java
@@ -0,0 +1,10 @@
+package Controller;
+
+public class kaasRelaasMain {
+
+	public static void main(String[] args) {
+		System.out.println("Dit programma doet nog niks, behalve deze output!");
+
+	}
+
+}
