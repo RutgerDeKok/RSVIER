6ae5651ee27e6ad2776ede82ac1f6fd521531e12
Rutger de Kok <de.kok.rutger@gmail.com>
2016-12-20 02:15:27 +0100

    Menu structuur powerpoint overzicht gemaakt.

--- Workshop1/KaasRelaas/src/Controller/KaasRelaasMain.java
+++ Workshop1/KaasRelaas/src/Controller/KaasRelaasMain.java
@@ -1,6 +1,6 @@
 package Controller;
 
-public class kaasRelaasMain {
+public class KaasRelaasMain {
 
 	public static void main(String[] args) {
 		System.out.println("Dit programma doet nog niks, behalve deze output!");
